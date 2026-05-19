bc518b73f76d639d2f3c049edaa239d1dc9a8f53
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-19 12:54:54 +0200

    FEAT: Added unimplemented global exception handler

---
+++ src/main/java/it/itsacademy/springsecurityjwt/interceptors/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package it.itsacademy.springsecurityjwt.interceptors;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    // TODO: Gestione eccezione: MethodArgumentNotValidException
+}
