afbd6350c5f6a29f2e6bd7c6ce6f99e304851201
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-19 11:26:30 +0200

    CHORE: Disabled Spring Security autoconfiguration

--- src/main/java/it/itsacademy/springsecurityjwt/SpringSecurityJwtApplication.java
+++ src/main/java/it/itsacademy/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -2,8 +2,10 @@ package it.itsacademy.springsecurityjwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class SpringSecurityJwtApplication {
 
     public static void main(String[] args) {
