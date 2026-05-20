ec054951dad379fe25c8c729824a0998577c14c1
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-20 12:16:49 +0200

    FEAT: Added new method to take a Utente from the database and convert it into a UserDetails

---
+++ src/main/java/it/itsacademy/springsecurityjwt/security/CustomUserDetailsService.java
@@ -0,0 +1,48 @@
+package it.itsacademy.springsecurityjwt.security;
+
+import it.itsacademy.springsecurityjwt.entity.Ruolo;
+import it.itsacademy.springsecurityjwt.entity.Utente;
+import it.itsacademy.springsecurityjwt.repository.UtenteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+import java.util.Set;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UtenteRepository utenteRepository;
+
+    /**
+     * Metodo helper per trasformare i ruoli (presenti nel db) in granted authorities (richieste da spring)
+     */
+    private List<?extends GrantedAuthority> ruoliToGrantedAuthorities(Set<Ruolo> ruoli) {
+        // Crea una lista di authorities a partire da un array di stringhe
+        return AuthorityUtils.createAuthorityList(
+                ruoli.stream()
+                .map(ruolo -> ruolo.getTipo().name()).toArray(String[]::new) // Prendi il tipo di ruolo e mettilo in un array di stringhe
+        );
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Cerca l'utente nel database
+        Utente utenteNelDB = utenteRepository.findByUsernameOrThrow(username);
+
+        // Convertilo "a mano" in uno user details di spring security
+        return new User(
+              utenteNelDB.getUsername(),
+              utenteNelDB.getPassword(),
+              utenteNelDB.getActive(),
+                true,
+                true,
+                !utenteNelDB.getActive(),
+                ruoliToGrantedAuthorities(utenteNelDB.getSetRuoli())
+        );
+    }
+}
