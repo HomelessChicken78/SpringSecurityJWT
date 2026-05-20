4dc76d819955e2f02ec567677ba3bc0f2c8ffd02
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-20 11:51:34 +0200

    FEAT: Added new method in repository to find a Utente by its username and if not found throw a custom exception

--- src/main/java/it/itsacademy/springsecurityjwt/repository/UtenteRepository.java
+++ src/main/java/it/itsacademy/springsecurityjwt/repository/UtenteRepository.java
@@ -1,6 +1,7 @@
 package it.itsacademy.springsecurityjwt.repository;
 
 import it.itsacademy.springsecurityjwt.entity.Utente;
+import it.itsacademy.springsecurityjwt.exception.NotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,6 +9,10 @@ import java.util.UUID;
 
 public interface UtenteRepository extends JpaRepository<Utente, UUID> {
     Optional<Utente> findByUsername(String username);
+    default Utente findByUsernameOrThrow(String username) {
+        return findByUsername(username)
+                .orElseThrow(() -> new NotFoundException("Utente con username \"" + username + "\" non trovato."));
+    }
     boolean existsByUsername(String username);
     Optional<Utente> findByIdUtente(UUID idUtente);
 }
