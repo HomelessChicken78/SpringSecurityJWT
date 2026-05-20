f661fa6157f8e4be07295e344e021eb4f9ef3682
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-20 16:11:32 +0200

    FIX: App crashing because of class not being registered as a bean and using unexisting imports

--- src/main/java/it/itsacademy/springsecurityjwt/security/CustomUserDetailsService.java
+++ src/main/java/it/itsacademy/springsecurityjwt/security/CustomUserDetailsService.java
@@ -10,10 +10,12 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Set;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UtenteRepository utenteRepository;
