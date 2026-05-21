fcc79233525ffcf0e7f99ac7a909ce571976c195
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-21 12:44:46 +0200

    CHORE: Configured CORS to allow all cross origins requests

--- src/main/java/it/itsacademy/springsecurityjwt/security/SecurityConfiguration.java
+++ src/main/java/it/itsacademy/springsecurityjwt/security/SecurityConfiguration.java
@@ -14,6 +14,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration // Indica a Spring: “La classe contiene la configurazione dei bean, cioè come creare oggetti gestiti da Spring.”
 @AllArgsConstructor
@@ -61,11 +66,24 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http // DI dell'HttpSecurityConfiguration grazie a EnableWebSecurity
     ) {
         http.csrf(AbstractHttpConfigurer::disable) // Disabilitiamolo poichè inutile per le API stateless
-        // TODO abilitare il CORS per postman
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests( // Definiamo le regole di accesso per gli URL
                 // NB: le regole più specifiche vanno più in alto o bloccherebbe le altre
                   a ->
