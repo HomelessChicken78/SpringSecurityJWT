5dcc0b4af993b02fde7352e8b5477cdf4a48ad76
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-21 10:09:48 +0200

    REFACTOR: Renamed endpoints /api/api/utenti/** to /api/utenti to make them less redundant

--- src/main/java/it/itsacademy/springsecurityjwt/controller/UtenteController.java
+++ src/main/java/it/itsacademy/springsecurityjwt/controller/UtenteController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping(path = "/api/utenti")
+@RequestMapping(path = "/utenti")
 @RequiredArgsConstructor
 public class UtenteController {
     private final UtenteService service;
